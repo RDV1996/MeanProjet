5d482a2fefc87d83292835cead7ad10c6d8e7c92
Unknown <roby1996_10@hotmail.com>
2018-08-25 17:04:59 +0200

    test posts

--- routes/post.js
+++ routes/post.js
@@ -29,76 +29,164 @@ router.post('/', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-    Post.find().exec(function (err, posts) {
-        if (err) {
-            return res.status(500).json({
-                title: 'Er heeft zich een fout voorgedaan',
-                error: err
+    //By title
+    if (req.query.title) {
+        Pagina.find({
+            title : new RegExp(req.params.title.replace(/\s+/g, "\\s+"), "i")
+        },function(err, posts){
+            if (err){
+                return res.status(500).json({
+                    title: 'Er heeft zich een fout voorgedaan',
+                    error: err
+                });
+            }
+            posts.sort(function(a, b){
+                var keyA = new Date(a.madeOn),
+                    keyB = new Date(b.madeOn);
+                // Compare the 2 dates
+                if(keyA < keyB) return -1;
+                if(keyA > keyB) return 1;
+                return 0;
             });
-        }
-        posts.sort(function(a, b){
-            var keyA = new Date(a.madeOn),
-                keyB = new Date(b.madeOn);
-            // Compare the 2 dates
-            if(keyA < keyB) return -1;
-            if(keyA > keyB) return 1;
-            return 0;
-        });
-        var result = [];
-        var end;
-        var start;
-        var temp = [];
-        var page = req.query.page;
-        if (req.query.sub) {
-            for (var i = 0; i < posts.length; i++) {
-                if (posts[i].pagina == req.query.sub) {
-                    temp.push(posts[i]);
-                }
-            }
-        }
-        else if (req.query.user) {
-            for (var i = 0; i < posts.length; i++) {
-                if (posts[i].user == req.query.user) {
-                    temp.push(posts[i]);
-                }
-            }
-        }
-        if (req.query.sub) {
-            for (var i = 0; i < posts.length; i++) {
-                if (posts[i].pagina == req.query.name) {
-                    temp.push(posts[i]);
-                }
-            }
-        }
-        else {
+            var result = [];
+            var end;
+            var start;
+            var temp = [];
+            var page = req.query.page;
             temp = posts;
-        }
-        var pages = Math.floor(temp.length / 20) + 1;
-        if (req.query.page == null || pages < page || page < 0) {
-            page = 1;
-        }
+            var pages = Math.floor(temp.length / 20) + 1;
+            if (req.query.page == null || pages < page || page < 0) {
+                page = 1;
+            }
 
 
-        if (temp.length > 20) {
-            end = (page * 20) - 1;
-            start = end - 20;
-        }
-        else {
-            end = temp.length - 1;
-            start = 0;
-        }
+            if (temp.length > 20) {
+                end = (page * 20) - 1;
+                start = end - 20;
+            }
+            else {
+                end = temp.length - 1;
+                start = 0;
+            }
 
-        for (var i = start; i <= end; i++) {
-            result.push(temp[i]);
-        }
+            for (var i = start; i <= end; i++) {
+                result.push(temp[i]);
+            }
 
 
-        res.status(200).json({
-            result: result,
-            page: page,
-            pages: pages
+            res.status(200).json({
+                result: result,
+                page: page,
+                pages: pages
+            });
         });
-    })
+    }
+    //By sub
+    if (req.query.sub) {
+        Pagina.find({
+            pagina : new RegExp(req.params.sub.replace(/\s+/g, "\\s+"), "i")
+        },function(err, posts){
+            if (err){
+                return res.status(500).json({
+                    title: 'Er heeft zich een fout voorgedaan',
+                    error: err
+                });
+            }
+            posts.sort(function(a, b){
+                var keyA = new Date(a.madeOn),
+                    keyB = new Date(b.madeOn);
+                // Compare the 2 dates
+                if(keyA < keyB) return -1;
+                if(keyA > keyB) return 1;
+                return 0;
+            });
+            var result = [];
+            var end;
+            var start;
+            var temp = [];
+            var page = req.query.page;
+            temp = posts;
+            var pages = Math.floor(temp.length / 20) + 1;
+            if (req.query.page == null || pages < page || page < 0) {
+                page = 1;
+            }
+
+
+            if (temp.length > 20) {
+                end = (page * 20) - 1;
+                start = end - 20;
+            }
+            else {
+                end = temp.length - 1;
+                start = 0;
+            }
+
+            for (var i = start; i <= end; i++) {
+                result.push(temp[i]);
+            }
+
+
+            res.status(200).json({
+                result: result,
+                page: page,
+                pages: pages
+            });
+        });
+    }
+    //By user
+    if (req.query.user) {
+        Pagina.find({
+            user : new RegExp(req.params.user.replace(/\s+/g, "\\s+"), "i")
+        },function(err, posts){
+            if (err){
+                return res.status(500).json({
+                    title: 'Er heeft zich een fout voorgedaan',
+                    error: err
+                });
+            }
+            posts.sort(function(a, b){
+                var keyA = new Date(a.madeOn),
+                    keyB = new Date(b.madeOn);
+                // Compare the 2 dates
+                if(keyA < keyB) return -1;
+                if(keyA > keyB) return 1;
+                return 0;
+            });
+            var result = [];
+            var end;
+            var start;
+            var temp = [];
+            var page = req.query.page;
+            temp = posts;
+            var pages = Math.floor(temp.length / 20) + 1;
+            if (req.query.page == null || pages < page || page < 0) {
+                page = 1;
+            }
+
+
+            if (temp.length > 20) {
+                end = (page * 20) - 1;
+                start = end - 20;
+            }
+            else {
+                end = temp.length - 1;
+                start = 0;
+            }
+
+            for (var i = start; i <= end; i++) {
+                result.push(temp[i]);
+            }
+
+
+            res.status(200).json({
+                result: result,
+                page: page,
+                pages: pages
+            });
+        });
+    }
+
+
 });
 
 router.get('/:id', function (req, res, next) {
